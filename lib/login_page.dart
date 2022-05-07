0268c43f7f3240fd7942e49fc0ab0d50d3178abe
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-05-07 17:14:00 +0800

    add:动画组件包   animations: ^2.0.2基本使用方法

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,6 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:getwidget/shape/gf_button_shape.dart';
-import './widgets/vent_button.dart';
+import './home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -19,14 +19,28 @@ class _LoginPageState extends State<LoginPage> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          VTextIconButton(
-            onPressed: () {},
-            buttonText: "Login",
-            buttonShape: GFButtonShape.pills,
-            icon: const Icon(
-              Icons.login,
-            ),
-          ),
+          OpenContainer(
+            //过度时长
+            transitionDuration: const Duration(seconds: 3),
+            //打开时的过度颜色
+            openColor: Colors.grey,
+            //动画类型
+            transitionType: ContainerTransitionType.fadeThrough,
+
+            closedBuilder: (context, action) {
+              return const Text(
+                "Open new HomePage",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              );
+            },
+            openBuilder: (context, action) {
+              //Scaffold顶部会自带一个返回按钮
+              return Scaffold(
+                appBar: AppBar(),
+                body: const HomePage(),
+              );
+            },
+          )
         ],
       ),
     );
