504243ab1d2ddad7c14dcfe92ed53be12a5dd582
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-05-18 19:14:57 +0800

    add:封装颜色代码

---
+++ lib/colors/vcolors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VColors {
+  //导航栏背景色
+  static const Color vGray100 = Color.fromRGBO(21, 25, 28, 1);
+  //页面背景色
+  static const Color vGray90 = Color.fromRGBO(29, 35, 42, 1);
+  //次要选项背景色
+  static const Color vGray80 = Color.fromRGBO(44, 52, 58, 1);
+  //重要文本色
+  //次要文本色
+}
