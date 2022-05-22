4fb3e94a4c40942c18d7f760000d1fcf9cd710bb
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-05-22 21:00:51 +0800

    fix:手机状态栏透明色

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './app.dart';
 
-void main() => runApp(const App());
+void main() {
+  //顶部状态栏透明
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(const App());
+}
