3ea9db56ab555d5022019dad6f212673b4adbe0b
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-05-23 18:11:57 +0800

    delete:删除部分注释

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import './widgets/vent_button.dart';
-import './vcolors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,97 +11,23 @@ class _HomePageState extends State<HomePage> {
   int a = 18;
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        VTextButton(
-          fullButton: false,
-          textColor: VColors.vPtext,
-          buttonColor: VColors.vBg100,
-          onPressed: () {},
-        ),
-      ],
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          Text(
+            "主页面",
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            "(制作中)...",
+            style: TextStyle(fontSize: 26),
+          )
+        ],
+      ),
     );
   }
 }
-
-// DefaultTextStyle(
-//   style: const TextStyle(
-//     fontSize: 20.0,
-//     color: Colors.blue,
-//   ),
-//   child: AnimatedTextKit(
-//     animatedTexts: [
-//       WavyAnimatedText('Hello World'),
-//       WavyAnimatedText('Look at the waves'),
-//     ],
-//     isRepeatingAnimation: true,
-//     repeatForever: true,
-//     onTap: () {},
-//   ),
-// ),
-//   a > 0
-//       ? Badge(
-//           badgeContent: Text(
-//             a.toString(),
-//             style: const TextStyle(color: Colors.white),
-//           ),
-//           animationType: BadgeAnimationType.slide,
-//           animationDuration: const Duration(milliseconds: 500),
-//           child: VTextIconButton(
-//             buttonText: "WeChat",
-//             buttonColor: Colors.green.shade400,
-//             onPressed: () {
-//               /// 延迟函数  记得在函数前加上async
-//               // await Future.delayed(
-//               //   const Duration(seconds: 1),
-//               // );
-//               setState(() {
-//                 a -= 1;
-//               });
-//             },
-//             icon: const Icon(
-//               Icons.wechat,
-//               color: Colors.white,
-//             ),
-//           ),
-//         )
-//       : VTextIconButton(
-//           buttonText: "WeChat",
-//           buttonColor: Colors.green.shade400,
-//           onPressed: () {
-//             /// 延迟函数  记得在函数前加上async
-//             // await Future.delayed(
-//             //   const Duration(seconds: 1),
-//             // );
-//             setState(() {
-//               a -= 1;
-//             });
-//           },
-//           icon: const Icon(
-//             Icons.wechat,
-//             color: Colors.white,
-//           ),
-//         ),
-//   VFTextIconButton(
-//     buttonText: "FaceBook",
-//     onPressed: () {
-//       vAlert(context, const Text("你好哇，我是标题"),
-//           const Text("你可以试着修改我的内容喔"));
-//     },
-//     buttonShape: GFButtonShape.pills,
-//     faIcon: const FaIcon(FontAwesomeIcons.facebook),
-//   ),
-//   VIconButton(
-//     onPressed: () {
-//       Fluttertoast.showToast(
-//         msg: "消息通知: xxxxx",
-//         textColor: Colors.white,
-//         backgroundColor: Colors.red.shade400,
-//         gravity: ToastGravity.TOP,
-//         fontSize: 20,
-//       );
-//     },
-//     icon: const Icon(Icons.settings),
-//     buttonShape: GFIconButtonShape.circle,
-//   ),
-//
