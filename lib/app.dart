5921fbbb6c3a379e2528d597c2cf9ca6e2999a0f
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-06-08 21:09:08 +0800

    change:切换页面触发VSnack函数(待定)

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:animations/animations.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ventroar_app/functions/vent_snack.dart';
 import 'package:ventroar_app/ventroar_bottom_bar.dart';
 import './drawer.dart';
 import './pages/home_page.dart';
@@ -26,6 +27,29 @@ class _AppState extends State<App> {
     const ChatListPage(),
     const LoginPage(),
   ];
+
+  String? getPageName(index) {
+    switch (index) {
+      case 0:
+        {
+          return "APP主页";
+        }
+      case 1:
+        {
+          return "星墙页面";
+        }
+      case 2:
+        {
+          return "聊天列表";
+        }
+      case 3:
+        {
+          return "登录页面";
+        }
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -114,6 +138,19 @@ class _AppState extends State<App> {
               setState(() {
                 _selectedIndex = index;
               });
+              vSnackBar(
+                key: GlobalKey(),
+                context: context,
+                textWidget: Text(
+                  getPageName(index) ?? "页面读取错误",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                showTime: const Duration(seconds: 2),
+              );
             },
           );
         }),
