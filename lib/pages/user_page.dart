a13df1a621d8ad2083391f74b7d89d34ac8860f1
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-06-09 15:12:26 +0800

    delete:注释

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -8,9 +8,6 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
-  // bool _value1 = false;
-  // bool _value2 = false;
-  // bool _value3 = false;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -24,64 +21,3 @@ class _UserPageState extends State<UserPage> {
     );
   }
 }
-
-// //利用Padding组件来设置与其他组件之间的间距
-// const Padding(
-//   padding: EdgeInsets.fromLTRB(0, 80, 0, 80),
-//   child: VSlidable(),
-// ),
-// const VSlidable(),
-// VCheckBoxList(
-//   isCheck: _value1,
-//   onChanged: (value) {
-//     setState(() {
-//       _value1 = value!;
-//     });
-//   },
-//   title: Text(
-//     "我是一个选择框功能按钮",
-//     style: TextStyle(color: VColors().vPtext),
-//   ),
-//   secondary: FaIcon(
-//     FontAwesomeIcons.facebook,
-//     color: VColors().vPtext,
-//   ),
-//   checkColor: VColors().vPtext,
-//   activeColor: VColors().vPblue,
-// ),
-// VCheckBoxList(
-//   isCheck: _value2,
-//   onChanged: (value) {
-//     setState(() {
-//       _value2 = value!;
-//     });
-//   },
-//   title: Text(
-//     "我是一个选择框功能按钮",
-//     style: TextStyle(color: VColors().vPtext),
-//   ),
-//   secondary: FaIcon(
-//     FontAwesomeIcons.twitter,
-//     color: VColors().vPtext,
-//   ),
-//   activeColor: VColors().vPblue,
-//   checkColor: VColors().vPtext,
-// ),
-// VCheckBoxList(
-//   isCheck: _value3,
-//   onChanged: (value) {
-//     setState(() {
-//       _value3 = value!;
-//     });
-//   },
-//   title: Text(
-//     "我是一个选择框功能按钮",
-//     style: TextStyle(color: VColors().vPtext),
-//   ),
-//   secondary: FaIcon(
-//     FontAwesomeIcons.amazon,
-//     color: VColors().vPtext,
-//   ),
-//   activeColor: VColors().vPblue,
-//   checkColor: VColors().vPtext,
-// ),
