3fb1132192f05483e62eea787e036415098d97b5
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-06-17 11:28:00 +0800

    change:优化侧边导航栏局内样式

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:provider/provider.dart';
 import 'package:ventroar_app/contexts/global_provider.dart';
+import 'package:ventroar_app/widgets/vent_button.dart';
 
 class VDrawer extends StatefulWidget {
   const VDrawer({
@@ -27,6 +29,8 @@ class _VDrawerState extends State<VDrawer> {
     int _selectedIndex = Provider.of<PageDataProvider>(context).selectedIndex;
 
     Map _pageDatas = Provider.of<PageDataProvider>(context).pageDatas;
+
+    bool _isDark = Provider.of<ThemeProvider>(context).isDark;
 
     List<Widget> _listTiles = [
       ListTile(
@@ -117,6 +121,48 @@ class _VDrawerState extends State<VDrawer> {
                 const Divider(
                   color: Colors.grey,
                 ),
+                SizedBox(
+                  height: 350,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        bottom: 8,
+                        left: 10,
+                        child: VTextIconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.output,
+                            color: Colors.redAccent,
+                            size: 32,
+                          ),
+                          buttonText: "退出登录",
+                          buttonType: GFButtonType.transparent,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        right: 10,
+                        child: IconButton(
+                          onPressed: () {
+                            Provider.of<ThemeProvider>(context, listen: false)
+                                .changeToDark(!_isDark);
+                          },
+                          splashColor: Colors.white.withOpacity(0),
+                          hoverColor: Colors.white.withOpacity(0),
+                          focusColor: Colors.white.withOpacity(0),
+                          disabledColor: Colors.white.withOpacity(0),
+                          highlightColor: Colors.white.withOpacity(0),
+                          tooltip: "切换主题",
+                          icon: Icon(
+                            _isDark
+                                ? FontAwesomeIcons.cloudSun
+                                : FontAwesomeIcons.cloudMoon,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
@@ -135,67 +181,39 @@ class DHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool _isDark = Provider.of<ThemeProvider>(context).isDark;
     return Container(
       height: 120,
-      padding: const EdgeInsets.fromLTRB(20, 5, 2, 0),
+      padding: const EdgeInsets.fromLTRB(10, 5, 0, 0),
       child: Stack(
         children: [
           Padding(
-            padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+            padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(25),
+              borderRadius: BorderRadius.circular(50),
               child: Image.asset("images/t2.png"),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(120, 0, 0, 0),
-            child: ListTile(
-              contentPadding: const EdgeInsets.all(0),
-              minLeadingWidth: 0,
-              title: Text(
-                _userName,
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(120, 35, 0, 0),
-            child: ListTile(
-              minLeadingWidth: 0,
-              contentPadding: const EdgeInsets.all(0),
-              title: Text(
-                _uesrEmail,
-                style: const TextStyle(
-                  fontSize: 15,
-                ),
+          Positioned(
+            top: 10,
+            right: 25,
+            child: Text(
+              _userName,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
           Positioned(
-            bottom: 1,
-            right: 15,
-            child: IconButton(
-              onPressed: () {
-                Provider.of<ThemeProvider>(context, listen: false)
-                    .changeToDark(!_isDark);
-              },
-              splashColor: Colors.white.withOpacity(0),
-              hoverColor: Colors.white.withOpacity(0),
-              focusColor: Colors.white.withOpacity(0),
-              disabledColor: Colors.white.withOpacity(0),
-              highlightColor: Colors.white.withOpacity(0),
-              tooltip: "切换主题",
-              icon: Icon(
-                _isDark
-                    ? FontAwesomeIcons.cloudSun
-                    : FontAwesomeIcons.cloudMoon,
+            top: 45,
+            right: 25,
+            child: Text(
+              _uesrEmail,
+              style: const TextStyle(
+                fontSize: 15,
               ),
             ),
-          )
+          ),
         ],
       ),
     );
