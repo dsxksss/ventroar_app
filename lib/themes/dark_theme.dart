beb21f68357addaab4302d7c57693bd9d892cdcb
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-06-29 15:23:54 +0800

    add:日/夜全局样式注明

--- lib/themes/dark_theme.dart
+++ lib/themes/dark_theme.dart
@@ -6,47 +6,116 @@ import './vcolors.dart';
 ThemeData darkTheme(BuildContext context) {
   final base = ThemeData.dark();
   return base.copyWith(
+    //顶部导航栏样式
     appBarTheme: AppBarTheme(
+      //景深或阴影深度
       elevation: 0,
+      //顶部导航栏字体样式
       titleTextStyle: GoogleFonts.zcoolKuaiLe(
-          fontWeight: FontWeight.w500, fontSize: 22, color: VColors.vPtext),
-      backgroundColor: VDarkThemeColors.appBarColors["bg"],
-      foregroundColor: VDarkThemeColors.appBarColors["foregroundColor"],
-    ),
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-      backgroundColor: const Color.fromARGB(148, 0, 174, 243),
-      foregroundColor: VColors.vPtext,
-    ),
-    iconTheme: IconThemeData(
-      color: VDarkThemeColors.iconColor,
-    ),
-    canvasColor: VDarkThemeColors.pageBG,
-    backgroundColor: VDarkThemeColors.pageBG,
-    splashColor: Colors.white.withOpacity(0),
-    highlightColor: Colors.white.withOpacity(0),
-    drawerTheme: DrawerThemeData(
-      backgroundColor: VDarkThemeColors.drawerColors["bg"],
-    ),
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      elevation: 0,
-      selectedItemColor: VDarkThemeColors.bottomNavColors["selectedItemColor"],
-      unselectedItemColor:
-          VDarkThemeColors.bottomNavColors["unselectedItemColor"],
-      backgroundColor: VDarkThemeColors.bottomNavColors["bg"],
-      selectedIconTheme: null,
-      unselectedIconTheme: null,
-      selectedLabelStyle: null,
-    ),
-    textTheme: TextTheme(
-      bodyText1: TextStyle(
+        //字粗
+        fontWeight: FontWeight.w500,
+        //字号
+        fontSize: 22,
+        //字色
         color: VColors.vPtext,
       ),
+      //背景色
+      backgroundColor: VDarkThemeColors.appBarColors["bg"],
+      //前景色
+      foregroundColor: VDarkThemeColors.appBarColors["foregroundColor"],
     ),
+
+    //悬浮按钮样式
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      //背景色
+      backgroundColor: const Color.fromARGB(148, 0, 174, 243),
+      //前景色
+      foregroundColor: VColors.vPtext,
+    ),
+
+    //icon图标样式
+    iconTheme: IconThemeData(
+      //图标色
+      color: VDarkThemeColors.iconColor,
+    ),
+
+    //画布色
+    canvasColor: VDarkThemeColors.pageBG,
+    //背景色
+    backgroundColor: VDarkThemeColors.pageBG,
+    //水波色
+    splashColor: Colors.white.withOpacity(0),
+    //高亮项内容色
+    highlightColor: Colors.white.withOpacity(0),
+
+    //侧边导航栏样式
+    drawerTheme: DrawerThemeData(
+      //背景色
+      backgroundColor: VDarkThemeColors.drawerColors["bg"],
+      //模态幕布色
+      // scrimColor: Colors.red,
+    ),
+
+    //底部导航栏样式
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      //景深或阴影深度
+      elevation: 0,
+      //选中色
+      selectedItemColor: VDarkThemeColors.bottomNavColors["selectedItemColor"],
+      //未选中色
+      unselectedItemColor:
+          VDarkThemeColors.bottomNavColors["unselectedItemColor"],
+      //背景色
+      backgroundColor: VDarkThemeColors.bottomNavColors["bg"],
+      //选中icon样式
+      selectedIconTheme: null,
+      //未选中icon样式
+      unselectedIconTheme: null,
+      //选中label样式
+      selectedLabelStyle: null,
+    ),
+
+    ///全局字体设置
+    textTheme: TextTheme(
+      //1号标题字样式
+      headline1: TextStyle(color: VColors.vPtext),
+      //2号标题字样式
+      headline2: TextStyle(color: VColors.vPtext),
+      //3号标题字样式
+      headline3: TextStyle(color: VColors.vPtext),
+      //4号标题字样式
+      headline4: TextStyle(color: VColors.vPtext),
+      //5号标题字样式
+      headline5: TextStyle(color: VColors.vPtext),
+      //6号标题字样式
+      headline6: TextStyle(color: VColors.vPtext),
+      //1号副标题字样式
+      subtitle1: TextStyle(color: VColors.vPtext),
+      //2号副标题字样式
+      subtitle2: TextStyle(color: VColors.vPtext),
+      //1号内容字样式
+      bodyText1: TextStyle(color: VColors.vPtext),
+      //2号内容字样式
+      bodyText2: TextStyle(color: VColors.vPtext),
+      //说明文字样式
+      caption: TextStyle(color: VColors.vPtext),
+      //按钮字体样式
+      button: TextStyle(color: VColors.vPtext),
+      //字体划线样式
+      overline: TextStyle(color: VColors.vPtext),
+    ),
+
+    //tile样式
     listTileTheme: ListTileThemeData(
+      //tile内容背景色
       tileColor: VDarkThemeColors.listTileColors["tileColor"],
+      //tile图标色
       iconColor: VDarkThemeColors.listTileColors["iconColor"],
+      //tile文字色
       textColor: VDarkThemeColors.listTileColors["textColor"],
+      //tile被选中色
       selectedColor: VDarkThemeColors.listTileColors["selectedColor"],
+      //tile内容被选中色
       selectedTileColor: VDarkThemeColors.listTileColors["selectedTileColor"],
     ),
   );
--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -6,49 +6,116 @@ import './vcolors.dart';
 ThemeData lightTheme(BuildContext context) {
   final base = ThemeData.light();
   return base.copyWith(
+    //顶部导航栏样式
     appBarTheme: AppBarTheme(
+      //景深或阴影深度
       elevation: 0,
+      //顶部导航栏字体样式
       titleTextStyle: GoogleFonts.zcoolKuaiLe(
-          fontWeight: FontWeight.w500, fontSize: 22, color: VColors.vBg100),
+        //字粗
+        fontWeight: FontWeight.w500,
+        //字号
+        fontSize: 22,
+        //字色
+        color: VColors.vBg100,
+      ),
+      //背景色
       backgroundColor: VLightThemeColors.appBarColors["bg"],
+      //前景色
       foregroundColor: VLightThemeColors.appBarColors["foregroundColor"],
     ),
+
+    //悬浮按钮样式
     floatingActionButtonTheme: FloatingActionButtonThemeData(
+      //背景色
       backgroundColor: VColors.vSblue,
+      //前景色
       foregroundColor: VColors.vPtext,
     ),
+
+    //icon图标样式
     iconTheme: IconThemeData(
+      //图标色
       color: VLightThemeColors.iconColor,
     ),
+
+    //画布色
     canvasColor: VLightThemeColors.pageBG,
+    //背景色
     backgroundColor: VLightThemeColors.pageBG,
+    //水波色
     splashColor: Colors.white.withOpacity(0),
+    //高亮项内容色
     highlightColor: Colors.white.withOpacity(0),
+
+    //侧边导航栏样式
     drawerTheme: DrawerThemeData(
+      //背景色
       backgroundColor: VLightThemeColors.drawerColors["bg"],
       //模态幕布颜色
       // scrimColor: Colors.red,
     ),
+
+    //底部导航栏样式
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      //景深或阴影深度
       elevation: 0,
+      //选中色
       selectedItemColor: VLightThemeColors.bottomNavColors["selectedItemColor"],
+      //未选中色
       unselectedItemColor:
           VLightThemeColors.bottomNavColors["unselectedItemColor"],
+      //背景色
       backgroundColor: VLightThemeColors.bottomNavColors["bg"],
+      //选中icon样式
       selectedIconTheme: null,
+      //未选中icon样式
       unselectedIconTheme: null,
+      //选中label样式
       selectedLabelStyle: null,
     ),
+
+    ///全局字体设置
     textTheme: TextTheme(
-      bodyText1: TextStyle(
-        color: VColors.vBg100,
-      ),
+      //1号标题字样式
+      headline1: TextStyle(color: VColors.vBg100),
+      //2号标题字样式
+      headline2: TextStyle(color: VColors.vBg100),
+      //3号标题字样式
+      headline3: TextStyle(color: VColors.vBg100),
+      //4号标题字样式
+      headline4: TextStyle(color: VColors.vBg100),
+      //5号标题字样式
+      headline5: TextStyle(color: VColors.vBg100),
+      //6号标题字样式
+      headline6: TextStyle(color: VColors.vBg100),
+      //1号副标题字样式
+      subtitle1: TextStyle(color: VColors.vBg100),
+      //2号副标题字样式
+      subtitle2: TextStyle(color: VColors.vBg100),
+      //1号内容字样式
+      bodyText1: TextStyle(color: VColors.vBg100),
+      //1号内容字样式
+      bodyText2: TextStyle(color: VColors.vBg100),
+      //说明文字样式
+      caption: TextStyle(color: VColors.vBg100),
+      //按钮字体样式
+      button: TextStyle(color: VColors.vBg100),
+      //字体划线样式
+      overline: TextStyle(color: VColors.vBg100),
     ),
+
+//tile样式
     listTileTheme: ListTileThemeData(
+      //tile内容背景色
       tileColor: VLightThemeColors.listTileColors["tileColor"],
+      //tile图标色
       iconColor: VLightThemeColors.listTileColors["iconColor"],
+      //tile文字色
       textColor: VLightThemeColors.listTileColors["textColor"],
+      //tile被选中色
       selectedColor: VLightThemeColors.listTileColors["selectedColor"],
+      //tile内容被选中色
       selectedTileColor: VLightThemeColors.listTileColors["selectedTileColor"],
     ),
   );
