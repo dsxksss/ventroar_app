e212de6f1cb54ea4978bb75b3dd5cf03170ca25d
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-06-30 18:08:37 +0800

    add:聊天页气泡样式调整

--- lib/message_bubble.dart
+++ lib/message_bubble.dart
@@ -1,5 +1,6 @@
 import 'package:chat_bubbles/bubbles/bubble_special_three.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ventroar_app/schemas/message.dart';
 
 class MessageBubble extends StatefulWidget {
@@ -15,21 +16,21 @@ class _MessageBubbleState extends State<MessageBubble> {
   Widget build(BuildContext context) {
     return Container(
       margin: widget.message.isSentByMe
-          ? const EdgeInsets.fromLTRB(0, 5, 0, 5)
+          ? const EdgeInsets.fromLTRB(0, 0, 0, 0)
           : const EdgeInsets.fromLTRB(0, 15, 0, 15),
       child: BubbleSpecialThree(
         text: widget.message.text,
         color: widget.message.isSentByMe
-            ? const Color(0xFF1B97F3)
-            : const Color(0xFFE8E8EE),
-        tail: false, //是否显示边角
+            ? const Color.fromARGB(255, 31, 148, 238)
+            : const Color.fromARGB(255, 243, 243, 253),
+        tail: true, //是否显示边角
         seen: false, //是否设置已读
         sent: false, //是否发送完毕
         delivered: false, //是否被选中
         isSender: widget.message.isSentByMe,
         textStyle: TextStyle(
           color: widget.message.isSentByMe ? Colors.white : Colors.black,
-          fontSize: 16,
+          fontSize: 15.sp,
         ),
       ),
     );
