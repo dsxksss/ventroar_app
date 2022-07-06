1cda949493201142d71ca07fcb47cb63030e7ad0
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-07-06 18:52:10 +0800

    add:实现基本登录功能(待优化待修复)

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../themes/vcolors.dart';
+import '../functions/vent_snack.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -16,14 +18,65 @@ class _LoginPageState extends State<LoginPage> {
     final TextEditingController _accountController = TextEditingController();
     final TextEditingController _passwordController = TextEditingController();
 
+    Future login() async {
+      try {
+        Response response;
+        var dio = Dio();
+        _accountController.text;
+        response =
+            await dio.post("https://ventroar.xyz:2546/userLoginApi/", data: {
+          "userPhoneNumber": _accountController.text,
+          "userPassword": _passwordController.text,
+        });
+        print(response);
+        print(response.headers["x-auth-token"]);
+        vSnackBar(
+          context: context,
+          model: VSnackModel.success,
+          textWidget: Text(
+            "登录成功",
+            style: TextStyle(fontSize: 15.sp, color: Colors.white),
+          ),
+          showTime: const Duration(milliseconds: 1400),
+        );
+        Future.delayed(
+          const Duration(seconds: 1),
+          () => {
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil("./", (route) => false)
+          },
+        );
+      } on DioError catch (e) {
+        if (e.response?.statusCode == 400) {
+          vSnackBar(
+            context: context,
+            model: VSnackModel.error,
+            textWidget: Text(
+              "账号或密码错误,请重试!",
+              style: TextStyle(fontSize: 15.sp, color: Colors.white),
+            ),
+          );
+        } else {
+          vSnackBar(
+            context: context,
+            model: VSnackModel.error,
+            textWidget: Text(
+              "网络繁忙,登录失败,请重试...",
+              style: TextStyle(fontSize: 15.sp, color: Colors.white),
+            ),
+          );
+        }
+      }
+    }
+
     return Builder(builder: (context) {
       return MediaQuery(
         data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
         child: Scaffold(
           body: Container(
+            color: Theme.of(context).canvasColor,
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
-            color: VColors.vBg100,
             child: Center(
               child: SizedBox(
                 height: MediaQuery.of(context).size.height,
@@ -35,23 +88,20 @@ class _LoginPageState extends State<LoginPage> {
                     TextFormField(
                       controller: _accountController,
                       decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Colors.white),
                         prefixIcon: Icon(
                           FontAwesomeIcons.solidUser,
-                          color: Colors.white,
                         ),
                       ),
                     ),
-                    const SizedBox(
-                      height: 20,
+                    SizedBox(
+                      height: 0.03.sh,
                     ),
                     TextFormField(
                       controller: _passwordController,
                       decoration: const InputDecoration(
-                        labelStyle: TextStyle(color: Colors.white),
+                        isDense: false,
                         prefixIcon: Icon(
                           FontAwesomeIcons.lock,
-                          color: Colors.white,
                         ),
                       ),
                     ),
@@ -60,8 +110,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     ElevatedButton(
                       onPressed: () {
-                        Navigator.of(context)
-                            .pushNamedAndRemoveUntil("./", (route) => false);
+                        login();
                       },
                       child: const Text("Login"),
                     ),
