306230af41f4e6735d376693b64c2ec49a93f81a
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-08-24 19:22:08 +0800

    add:头像组件封装

---
+++ lib/global/widgets/avatars.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../schemas/user.dart';
+
+class Avatar extends StatelessWidget {
+  Avatar({
+    Key? key,
+    borderRadius,
+    this.onPressed,
+    required user,
+  })  : _user = user,
+        super(key: key);
+
+  final User _user;
+  final VoidCallback? onPressed;
+  final BorderRadius? _borderRadius = BorderRadius.circular(999);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: ClipRRect(
+        borderRadius: _borderRadius,
+        child: _user.avatarUrl == "null"
+            ? Container(
+                color: Colors.blue[200],
+                child: Center(
+                  child: Text(
+                    _user.name[0],
+                    style: TextStyle(color: Colors.white, fontSize: 1.sp),
+                  ),
+                ))
+            : SizedBox(
+                child: Image.network(
+                    "https://ventroar.xyz:2548/avatars/${_user.avatarUrl}"),
+              ),
+      ),
+    );
+  }
+}
