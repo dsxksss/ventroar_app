c6f613ef79b28d9045c2ffd95d02f2d6fb449f6d
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-08-24 22:44:01 +0800

    add:头像组件增加fontsize参数项

--- lib/global/widgets/avatars.dart
+++ lib/global/widgets/avatars.dart
@@ -6,12 +6,14 @@ class Avatar extends StatelessWidget {
   Avatar({
     Key? key,
     borderRadius,
+    this.fontSize,
     this.onPressed,
     required user,
   })  : _user = user,
         super(key: key);
 
   final User _user;
+  final double? fontSize;
   final VoidCallback? onPressed;
   final BorderRadius? _borderRadius = BorderRadius.circular(999);
 
@@ -27,7 +29,8 @@ class Avatar extends StatelessWidget {
                 child: Center(
                   child: Text(
                     _user.name[0],
-                    style: TextStyle(color: Colors.white, fontSize: 1.sp),
+                    style: TextStyle(
+                        color: Colors.white, fontSize: fontSize ?? 26.sp),
                   ),
                 ))
             : SizedBox(
