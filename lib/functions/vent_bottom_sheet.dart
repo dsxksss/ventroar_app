d53284311624a656ce4653945380d7a6e13dd55c
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-09-17 17:03:47 +0800

    change:调整底部弹出页函数参数内容

--- lib/functions/vent_bottom_sheet.dart
+++ lib/functions/vent_bottom_sheet.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-vBottomSheet({required BuildContext context}) {
+vBottomSheet({
+  required Widget content,
+  required BuildContext context,
+  double? height,
+}) {
   showModalBottomSheet(
     context: context,
-    elevation: 0.65.sh,
-    anchorPoint: const Offset(100, 100),
     isScrollControlled: true,
-    backgroundColor: Theme.of(context).backgroundColor,
+    clipBehavior: Clip.none,
+    backgroundColor: Colors.transparent,
     shape: const RoundedRectangleBorder(
       //顶部圆角
       borderRadius: BorderRadius.vertical(
@@ -15,36 +18,47 @@ vBottomSheet({required BuildContext context}) {
       ),
     ),
     builder: (context) {
-      return Container(
-        clipBehavior: Clip.none,
-        height: 0.65.sh,
-        decoration: BoxDecoration(
-          //顶部圆角
-          borderRadius: const BorderRadius.vertical(
-            top: Radius.circular(20),
-          ),
-          color: Theme.of(context).backgroundColor,
-        ),
-        child: Stack(
-          clipBehavior: Clip.none,
-          alignment: AlignmentDirectional.center,
-          children: [
-            Positioned(
-              top: -0.013.sh,
-              child: Container(
-                decoration: BoxDecoration(
-                  //顶部圆角
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(99),
-                  ),
-                  color: Colors.grey.shade300,
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            alignment: AlignmentDirectional.center,
+            width: 1.sw,
+            child: Container(
+              decoration: BoxDecoration(
+                //顶部圆角
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(99),
                 ),
-                height: 0.01.sh,
-                width: 0.3.sw,
+                color: Colors.grey.shade300,
               ),
+              height: 0.01.sh,
+              width: 0.3.sw,
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 0.005.sh,
+          ),
+          Container(
+            clipBehavior: Clip.none,
+            height: height ?? 0.3.sh,
+            width: 1.sw,
+            constraints: BoxConstraints(
+              minHeight: 0.3.sh,
+              maxHeight: 0.8.sh,
+              minWidth: 1.sw,
+              maxWidth: 1.sw,
+            ),
+            decoration: BoxDecoration(
+              //顶部圆角
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(20),
+              ),
+              color: Theme.of(context).backgroundColor,
+            ),
+            child: content,
+          ),
+        ],
       );
     },
   );
