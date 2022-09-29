9d4845c95e270f06e73c622b505954c99d95885c
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-09-29 19:01:27 +0800

    change:调整部分样式

--- lib/global/widgets/post_roar_widget.dart
+++ lib/global/widgets/post_roar_widget.dart
@@ -82,110 +82,105 @@ class _PostRoarWidgetState extends State<PostRoarWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return AnimatedContainer(
+      duration: const Duration(seconds: 2),
       padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                onPressed: () {
-                  if (contentText.isNotEmpty) {
-                    vDialog(
-                      context: context,
-                      title: Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Icon(
-                            Icons.warning_amber_rounded,
-                            color: Colors.amber[800],
-                            size: 32.sp,
-                          ),
-                          Text(
-                            " 注 意",
-                            style: Theme.of(context).textTheme.headline3,
-                          ),
-                        ],
-                      ),
-                      contene: Text(
-                        "已编辑内容还未发布,退出会重新编辑,确定退出嘛?",
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      bottom: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text("确定"),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text("取消"),
-                          ),
-                        ],
-                      ),
-                    );
-                  } else {
-                    Navigator.of(context).pop();
-                  }
-                },
-                //去除按钮水波纹效果
-                splashColor: Colors.transparent,
-                icon: Icon(
-                  Icons.close_rounded,
-                  size: 30.sp,
+          AnimatedContainer(
+            duration: const Duration(seconds: 2),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    if (contentText.isNotEmpty) {
+                      vDialog(
+                        context: context,
+                        title: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.warning_amber_rounded,
+                              color: Colors.amber[800],
+                              size: 32.sp,
+                            ),
+                            Text(
+                              " 注 意",
+                              style: Theme.of(context).textTheme.headline3,
+                            ),
+                          ],
+                        ),
+                        contene: Text(
+                          "已编辑内容还未发布,退出会重新编辑,确定退出嘛?",
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        bottom: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text("确定"),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text("取消"),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else {
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  //去除按钮水波纹效果
+                  splashColor: Colors.transparent,
+                  icon: Icon(
+                    Icons.close_rounded,
+                    size: 30.sp,
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    width: 0.1.sw,
-                    height: 0.045.sh,
-                    child: AvatarWidget(
-                      avatarUrl: userBox.get("my")?.avatarUrl ?? "null",
-                      userName: userBox.get("my")?.name ?? "null",
-                    ),
-                  ),
-                  SizedBox(
-                    width: 0.02.sw,
-                  ),
-                  Container(
-                    constraints: BoxConstraints(
-                      minWidth: 0.sw,
-                      maxWidth: 0.35.sw,
-                    ),
-                    child: Text(
-                      userBox.get("my")?.name ?? "null",
-                      style: TextStyle(
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.bold,
-                        overflow: TextOverflow.ellipsis,
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      width: 0.1.sw,
+                      height: 0.045.sh,
+                      child: AvatarWidget(
+                        avatarUrl: userBox.get("my")?.avatarUrl ?? "null",
+                        userName: userBox.get("my")?.name ?? "null",
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Visibility(
-                //是否预加载
-                maintainState: true,
-                //是否预加载动画
-                maintainAnimation: true,
-                //是否占位
-                maintainSize: true,
-                visible: textEditingController.text.isNotEmpty ? true : false,
-                child: AnimatedOpacity(
-                  duration: const Duration(milliseconds: 200),
-                  opacity: textEditingController.text.isNotEmpty ? 1 : 0,
-                  child: ElevatedButton(
+                    SizedBox(
+                      width: 0.02.sw,
+                    ),
+                    Container(
+                      constraints: BoxConstraints(
+                        minWidth: 0.sw,
+                        maxWidth: 0.35.sw,
+                      ),
+                      child: Text(
+                        userBox.get("my")?.name ?? "null",
+                        style: TextStyle(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 0.02.sw,
+                    ),
+                  ],
+                ),
+                if (textEditingController.text.isNotEmpty)
+                  ElevatedButton(
                     onPressed: () {
                       postText();
                     },
@@ -207,9 +202,8 @@ class _PostRoarWidgetState extends State<PostRoarWidget> {
                       ),
                     ),
                   ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
           SizedBox(
             height: 0.01.sh,
